69edd1cf7a3160a50ea26fe07435997363e1ad7a
Aykrifa <aykrifa@student.42.fr>
2026-02-12 10:07:02 +0100

    ex00

--- ex00/header/easyfind.hpp
+++ ex00/header/easyfind.hpp
@@ -6,19 +6,18 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 11:01:29 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/09/18 14:27:03 by aykrifa          ###   ########.fr       */
+/*   Updated: 2026/02/12 10:00:00 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <stdexcept>
 
 template <typename T>
-typename T::iterator	easyfind(T container, int n)
+typename T::iterator	easyfind(T &container, int n)
 {
-	int	*p = std::find(container.begin(), container.end(), n);
-	if (p != container.end())
-		return (p);
-	throw (std::exception e);
+	typename T::iterator it = std::find(container.begin(), container.end(), n);
+	return (it);
 }
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,23 +6,66 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 11:21:00 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/09/18 13:50:29 by aykrifa          ###   ########.fr       */
+/*   Updated: 2026/02/12 10:05:01 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// vector::begin/end
+#include <exception>
 #include <iostream>
 #include <vector>
+#include "easyfind.hpp"
+
+void	tryWrongContainer(void)
+{
+	// std::vector<std::string> myvector;
+	// for (int i=1; i<=5; i++)
+	// 	myvector.push_back("a");
+	// try
+	// {
+	// 	std::vector<std::string>::iterator it;
+	//
+	// 	for (int i = -5; i < 10; i++)
+	// 	{
+	// 		it = easyfind(myvector, i);
+	// 		(it == myvector.end()) ? (std::cout << i << " not found !")
+	// 			: (std::cout << i << " caught :" << *it);
+	// 		std::cout << std::endl;
+	// 	}
+	// }
+	// catch (std::exception &e)
+	// {
+	// 	std::cerr << e.what() << std::endl;
+	// }
+}
 
 int main ()
 {
-  std::vector<int> myvector;
-  for (int i=1; i<=5; i++) myvector.push_back(i);
+	std::vector<int> myvector;
+	for (int i=1; i<=5; i++)
+		myvector.push_back(i);
 
-  std::cout << "myvector contains:";
-  for (std::vector<int>::iterator it = myvector.begin() ; it != myvector.end(); ++it)
-    std::cout << ' ' << *it;
-  std::cout << '\n';
+	std::cout << "myvector contains:";
+	for (std::vector<int>::iterator it = myvector.begin() ; it != myvector.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << std::endl;
 
-  return 0;
+	try
+	{
+		std::vector<int>::iterator it;
+	
+		for (int i = -5; i < 10; i++)
+		{
+			it = easyfind(myvector, i);
+			(it == myvector.end()) ? (std::cout << i << " not found !")
+				: (std::cout << i << " caught :" << *it);
+			std::cout << std::endl;
+		}
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	tryWrongContainer();
+	return (0);
 }
